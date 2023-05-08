2445b099ac9cdc97a13b6e9021544f8f3f801715
Maryna Zahorskaya <zagorskayam@mail.ru>
2023-05-08 14:23:09 +0300

    fix caching

--- src/main/java/com/incorparation/interceptors/CacheControlInterceptor.java
+++ src/main/java/com/incorparation/interceptors/CacheControlInterceptor.java
@@ -7,36 +7,36 @@ import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 @Component
 public class CacheControlInterceptor implements HandlerInterceptor {
-    private static final List<String> URL_WITH_CACHE = Arrays.asList("/storage/api/products/categories");
     private static final List<String> URL_NO_CACHE = Arrays.asList("/storage/api/products/test");
+    private static final List<String> URL_EXCLUDE_CACHE = Arrays.asList("/storage/api/products/categories");
+    //private static final List<String> URL_EXCLUDE_CACHE = new ArrayList<>();
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-        AntPathMatcher matcher = new AntPathMatcher();
-
         if (request.getMethod().equals("GET")) {
-            URL_WITH_CACHE.forEach(urlWithCache -> {
-                boolean isWithCache = matcher.match(urlWithCache, request.getRequestURI());
+            boolean isURLExclude = isURLMatch(URL_EXCLUDE_CACHE, request.getRequestURI());
+            boolean isURLNoCached = isURLMatch(URL_NO_CACHE, request.getRequestURI());
 
-                if (isWithCache) {
-                    response.addHeader("Cache-Control", CacheControl.maxAge(5, TimeUnit.HOURS).toString());
-                } else {
-                    response.addHeader("Cache-Control","no-cache");
-                }
-            });
-        } else {
-            if (!request.getMethod().equals("OPTIONS")) {
-                response.addHeader("Cache-Control", "no-cache");
+            if (isURLExclude) {
+                return true;
+            } else if (isURLNoCached) {
+                response.addHeader("Cache-Control","no-cache");
+            } else {
+                response.addHeader("Cache-Control", CacheControl.maxAge(5, TimeUnit.HOURS).toString());
             }
         }
 
         return true;
-        //return HandlerInterceptor.super.preHandle(request, response, handler);
+    }
+
+    private boolean isURLMatch(List<String> urls, String toFind) {
+        return urls.contains(toFind);
     }
 }
