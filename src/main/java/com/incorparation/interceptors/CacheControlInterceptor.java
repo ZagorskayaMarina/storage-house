fbf49cd82d29037fffe1ac04d5062ded1122766b
Maryna Zahorskaya <zagorskayam@mail.ru>
2023-05-09 21:46:33 +0300

    fix caching

--- src/main/java/com/incorparation/interceptors/CacheControlInterceptor.java
+++ src/main/java/com/incorparation/interceptors/CacheControlInterceptor.java
@@ -14,29 +14,31 @@ import java.util.concurrent.TimeUnit;
 
 @Component
 public class CacheControlInterceptor implements HandlerInterceptor {
-    private static final List<String> URL_NO_CACHE = Arrays.asList("/storage/api/products/test");
-    private static final List<String> URL_EXCLUDE_CACHE = Arrays.asList("/storage/api/products/categories");
-    //private static final List<String> URL_EXCLUDE_CACHE = new ArrayList<>();
+    private static final List<String> NO_CACHE_URL_PATTERN = Arrays.asList("/storage/api/products/test");
+    //private static final List<String> EXCLUDE_CACHE_URL_PATTERN = Arrays.asList("/storage/api/products/categories");
+    private static final List<String> EXCLUDE_CACHE_URL_PATTERN = new ArrayList<>();
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
         if (request.getMethod().equals("GET")) {
-            boolean isURLExclude = isURLMatch(URL_EXCLUDE_CACHE, request.getRequestURI());
-            boolean isURLNoCached = isURLMatch(URL_NO_CACHE, request.getRequestURI());
+            var isURLExclude = isURLMatch(EXCLUDE_CACHE_URL_PATTERN, request.getRequestURI());
+            var isURLNoCached = isURLMatch(NO_CACHE_URL_PATTERN, request.getRequestURI());
 
             if (isURLExclude) {
                 return true;
             } else if (isURLNoCached) {
                 response.addHeader("Cache-Control","no-cache");
             } else {
-                response.addHeader("Cache-Control", CacheControl.maxAge(5, TimeUnit.HOURS).toString());
+                response.addHeader("Cache-Control", CacheControl.maxAge(5, TimeUnit.HOURS).getHeaderValue());
             }
         }
 
         return true;
     }
 
-    private boolean isURLMatch(List<String> urls, String toFind) {
-        return urls.contains(toFind);
+    private boolean isURLMatch(List<String> patternList, String url) {
+        AntPathMatcher matcher = new AntPathMatcher();
+
+        return patternList.stream().anyMatch(pattern -> matcher.match(pattern, url));
     }
 }
