52fae82e33f103bd1421a5b1b142cb234dee3c0d
Maryna Zahorskaya <zagorskayam@mail.ru>
2023-06-04 20:21:31 +0300

    add CORS

--- src/main/java/com/incorparation/configuration/MvcConfiguration.java
+++ src/main/java/com/incorparation/configuration/MvcConfiguration.java
@@ -1,9 +1,19 @@
 package com.incorparation.configuration;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebMvc
-public class MvcConfiguration {
+public class MvcConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedMethods("POST", "PUT", "GET", "DELETE")
+                .allowedOrigins("*");
+    }
 }
